a1b6f019222124bad9f5e7844eda1367f67c964a
bakon <bakon@pipe.kitchen>
2024-12-06 05:55:38 -0600

    dont cert check on wss

--- server/typescript/src/indexer/indexer.ts
+++ server/typescript/src/indexer/indexer.ts
@@ -5,10 +5,10 @@ import { open } from 'sqlite';
 const indexerdb = "./src/indexer/indexer.db";
 console.log("indexerdb: ", indexerdb);
 
-console.log("web socket: ", process.env.OGMIOS_WS, {
+console.log("web socket: ", process.env.OGMIOS_WS);
+const client = new WebSocket(process.env.OGMIOS_WS as string, {
   rejectUnauthorized: false  // This bypasses certificate validation
 });
-const client = new WebSocket(process.env.OGMIOS_WS as string);
 console.log("client: ", client);
 
 const network = process.env.NETWORK;
